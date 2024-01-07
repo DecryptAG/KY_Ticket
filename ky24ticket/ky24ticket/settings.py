c2400a14cc9169425b4cdacde523fe0a3e4699dd
DecryptAG <akshat.goel.cd.eee21@itbhu.ac.in>
2024-01-07 16:12:42 +0000

    changes

--- ky24ticket/ky24ticket/settings.py
+++ ky24ticket/ky24ticket/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROD = False
+PROD = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
