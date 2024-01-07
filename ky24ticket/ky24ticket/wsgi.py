10592537ed48f14e5378989fb25682f1227e0626
DecryptAG <akshat.goel.cd.eee21@itbhu.ac.in>
2024-01-07 12:22:01 +0000

    changes

--- ky24ticket/ky24ticket/wsgi.py
+++ ky24ticket/ky24ticket/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
-from django.core.wsgi import get_wsgi_application
-
+sys.path.append('./path/to/venv/lib/python3.11/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ky24ticket.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
